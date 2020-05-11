1023edb802147cbe6ba87fbf041760c769b650d2
CARTOGIS33 <61816683+CARTOGIS33@users.noreply.github.com>
2020-05-11 15:46:51 +0200

    Add files via upload

---
+++ data/EQUIPES_3.js
@@ -0,0 +1,13 @@
+var json_EQUIPES_3 = {
+"type": "FeatureCollection",
+"name": "EQUIPES_3",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "id": null, "Equipe": "1" }, "geometry": { "type": "Point", "coordinates": [ -0.69166100138932, 44.749911034836501 ] } },
+{ "type": "Feature", "properties": { "id": null, "Equipe": "2" }, "geometry": { "type": "Point", "coordinates": [ -0.67975378600346, 44.739240831982016 ] } },
+{ "type": "Feature", "properties": { "id": null, "Equipe": null }, "geometry": { "type": "Point", "coordinates": [ -0.64592844296216, 44.775575997791812 ] } },
+{ "type": "Feature", "properties": { "id": null, "Equipe": "2" }, "geometry": { "type": "Point", "coordinates": [ -0.647540951444495, 44.777748434485716 ] } },
+{ "type": "Feature", "properties": { "id": null, "Equipe": null }, "geometry": { "type": "Point", "coordinates": [ -0.749094495281569, 44.806711494019638 ] } },
+{ "type": "Feature", "properties": { "id": null, "Equipe": "1" }, "geometry": { "type": "Point", "coordinates": [ -0.528113569088707, 44.817838149199254 ] } }
+]
+}
